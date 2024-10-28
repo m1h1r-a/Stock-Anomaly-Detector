f423914171d94dbfb5e8a4211b590d307140cecd
m1h1r-a <mihir.annambhotla@gmail.com>
2024-10-28 14:13:25 +0530

    css

--- users/forms.py
+++ users/forms.py
@@ -3,23 +3,53 @@ from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control mb-3",  # Bootstrap classes
+                "placeholder": "Username",
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control mb-3", "placeholder": "Password"}
+        )
+    )
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_confirm = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control mb-3", "placeholder": "Password"}
+        )
+    )
+    password_confirm = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control mb-3", "placeholder": "Confirm Password"}
+        )
+    )
 
     class Meta:
         model = User
         fields = ["username", "email", "password"]
+        widgets = {
+            "username": forms.TextInput(
+                attrs={"class": "form-control mb-3", "placeholder": "Username"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"class": "form-control mb-3", "placeholder": "Email"}
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].help_text = ""
 
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
         password_confirm = cleaned_data.get("password_confirm")
-
         if password != password_confirm:
             raise forms.ValidationError("Passwords do not match")
 
