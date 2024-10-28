d6a26865539d2ae69f2124743a49fd0732de9b3c
NandanaCA <nandanacanil@gmail.com>
2024-10-28 12:25:59 +0530

    mysql connectivity ig

--- main/settings.py
+++ main/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DBMS_project',  # Your database name
+        'USER': 'team5',    # The user created earlier
+        'PASSWORD': 'password',  # Password for the user
+        'HOST': 'localhost',       # Set to 'localhost'
+        'PORT': '3306',            # Default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
